46b3e06a303eaf2a6e03a27f01599a4846c43eff
shuvo-sarkaR-sys <md.sirforce@gmail.com>
2026-03-06 20:35:00 +0600

    push

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,7 +48,32 @@ export default function RootLayout({
   return (
     <html lang="en">
       <link rel="icon" href="/img/favicons/royal.ico" />
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TGQD4PSJ');`,
+          }}
+        />
+        {/* <!-- Google tag (gtag.js) --> */}
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-DNTJZ19XMJ"></script>
+<script dangerouslySetInnerHTML={{
+  __html: `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){window.dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-DNTJZ19XMJ');
+  `
+}} />
+      </head>
       <body className={`${inter.className} ${oswald.className}`}>
+       {/* Google Tag Manager (noscript) --> */}
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TGQD4PSJ"
+height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe></noscript>
+{/* <!-- End Google Tag Manager (noscript) --> */}
         <div
           className="absolute inset-0 bg-grid-slate-900/[0.04] bg-[bottom_1px_center] dark:bg-grid-slate-400/[0.05] dark:bg-bottom dark:border-b dark:border-slate-100/5 [mask-image:linear-gradient(0deg,transparent_0%,black_25%)] -z-10"
           style={{ backgroundImage: `url('/img/line.svg')` }}
